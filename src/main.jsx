38fdf9a6dbdfc4b04c4cd9edb4df9bf69b27eb52
Abhay Raj <abhayraj620285@gmail.com>
2026-04-15 16:20:22 +0530

    corrected main

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-git branch -M mainimport { StrictMode } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
